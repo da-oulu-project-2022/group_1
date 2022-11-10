92fc2b7cc7462a74863f9742f4bce00162cc2ece
Duong Pham <t0phtr00@students.oamk.fi>
2022-11-10 17:03:12 +0200

    Update
    Converting HTML to React

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -170,54 +170,53 @@ const onClickEvent = () => {
 
   return (
     <div class="back">
-        <div class="header">
-            <img class="logo" src="./polar (2).png"/>
-            <button id = "connectButton">Connect device</button>
+      <div class="header">
+        <img class="logo" src="./public/polarLogo.png"/>
+        <button id = "connectButton">Connect device</button>
+      </div>
+      <hr/>
+      <div class="stats-container">
+        <div class="small-column">
+          <div class="data dataText">0</div>
+          <div class="data dataUnit">khm/h</div>
         </div>
-        <hr/>
-        <div class="stats-container">
-            <div class="small-column">
-                <div class="data dataText">0</div>
-                <div class="data dataUnit">khm/h</div>
-            </div>
-            <div class="small-column">
-                <div id="clock" class="data dataText" onload="currentTime()">15:15</div>
-                
-            </div>
-            <div class="small-column">
-                <div class="data dataText">0</div>
-                <div class="data dataUnit">BPM</div>
-            </div>
+        <div class="small-column">
+          <div id="clock" class="data dataText" onload="currentTime()">15:15</div>
         </div>
-        <hr/>
-        <div class="stats-container">
-            <div class="small-column">
-                <div class="data dataText">60</div>
-                <div class="data dataUnit">Lowest BPM</div>
-            </div>
-            <div class="small-column">
-                <div>
+        <div class="small-column">
+          <div class="data dataText">0</div>
+          <div class="data dataUnit">BPM</div>
+        </div>
+      </div>
+      <hr/>
+      <div class="stats-container">
+        <div class="small-column">
+          <div class="data dataText">60</div>
+          <div class="data dataUnit">Lowest BPM</div>
+        </div>
+        <div class="small-column">
+          <div>
             <img id="heartSprite" src = "./heart.png" hidden="true"/>
             <div id="dataText" class="dataText">0</div>
             <div class="dataUnit">BPM</div>
-                </div>
-            </div>
-            <div class="small-column">
-                <div class="data dataText">120</div>
-                <div class="data dataUnit">Highest BPM</div>
-            </div>
+          </div>
         </div>
-        <hr/>
-        <div class="graph-container">
-            <div class="big-column">
-                <div id="chart_div_1" class="graph"></div>
-                <div class="graph-name2">ECG</div>
-            </div>
-            <div class="big-column">
-                <div id="chart_div_2" class="graph"></div>
-                <div class="graph-name">PPG</div>
-            </div>
+        <div class="small-column">
+          <div class="data dataText">120</div>
+          <div class="data dataUnit">Highest BPM</div>
         </div>
+      </div>
+      <hr/>
+      <div class="graph-container">
+        <div class="big-column">
+          <div id="chart_div_1" class="graph"></div>
+          <div class="graph-name2">ECG</div>
+        </div>
+        <div class="big-column">
+          <div id="chart_div_2" class="graph"></div>
+          <div class="graph-name">PPG</div>
+        </div>
+      </div>
     </div>
   );
 }
