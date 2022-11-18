983858676fce9e87d535f3dca1280c4bdbb14d18
Kooalaa <konsta.alajarvi@gmail.com>
2022-11-18 10:35:22 +0200

    asd

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -11,9 +11,19 @@ function App() {
   const [acceleration, setAcceleration] = useState(null);
   const [ecg, setEcg] = useState(null);
   const [device, setDevice] = useState(null);
+  const [server, setServer] = useState(null);
+  const [services, setServices] = useState(null);
+
+  const PMD_Service = "fb005c80-02e7-f387-1cad-8acd2d8df0c8";
+  const Heart_rate_Service = "0000180d-0000-1000-8000-00805f9b34fb";
+
+  const Cntrl_char = "fb005c81-02e7-f387-1cad-8acd2d8df0c8";
+  const Data_char = "fb005c82-02e7-f387-1cad-8acd2d8df0c8";
+  const Heart_rate_Char = "00002a37-0000-1000-8000-00805f9b34fb";
 
   // When the component mounts, check that the browser supports Bluetooth
   useEffect(() => {
+    console.log("updated");
     if (navigator.bluetooth) {
       setSupportsBluetooth(true);
     }
@@ -66,25 +76,50 @@ function App() {
       "fb005c80-02e7-f387-1cad-8acd2d8df0c8"
     ]
   }
+  
 
-const onClickEvent = () => {
+let heartRateChanged = (event) => {
+  console.log(event);
+}
+
+
+const connectDevice = () => {
   navigator.bluetooth.requestDevice(options)
+  .then(device => {
+    setDevice(device);
+    device.gatt.connect()
+    .then(server => {
+      setServer(server);
+      server.getPrimaryService(Heart_rate_Service)
+      .then(service => {
+        service.getCharacteristic(Heart_rate_Char)
+        .then(char => {
+          console.log(char);
+          console.log(char.properties);
+          char.addEventListener("changed", heartRateChanged);
+          char.startNotifications();
+        })
+      })
+    });
+  });
+/*   navigator.bluetooth.requestDevice(options)
     .then(device => {
       setDevice(device);
       return device.gatt.connect();
     })
     .then(server => {
-        return server.getPrimaryServices();
+      return server.getPrimaryServices();
     })
     .then(services => {
         console.log(services);
-        let queue = Promise.resolve();
+        // let queue = Promise.resolve();
         services.forEach(service => {
           //console.log(service)
             switch (service.uuid) {
               case "0000180f-0000-1000-8000-00805f9b34fb":
                 //batterylevel
-                service.getCharacteristic("00002a19-0000-1000-8000-00805f9b34fb").then( value =>{
+                service.getCharacteristic("00002a19-0000-1000-8000-00805f9b34fb")
+                .then(value => {
                   value.addEventListener('characteristicvaluechanged', batteryLevelChanged);
                     console.log(value);
                 });
@@ -92,7 +127,8 @@ const onClickEvent = () => {
                 break;
               case "0000180d-0000-1000-8000-00805f9b34fb":
                 //heartrate
-                service.getCharacteristic("00002a37-0000-1000-8000-00805f9b34fb").then( value =>{
+                service.getCharacteristic("00002a37-0000-1000-8000-00805f9b34fb")
+                .then(value => {
                   value.addEventListener('characteristicvaluechanged', heartRateChanged);
                   console.log(value);
               });
@@ -115,9 +151,13 @@ const onClickEvent = () => {
 
                     }).then (_ => {
                         service.getCharacteristic("fb005c81-02e7-f387-1cad-8acd2d8df0c8").then(controlCharacteristic => {
-                            //console.log(controlCharacteristic);
-                            //console.log("writing value to device");
-                            controlCharacteristic.writeValueWithoutResponse(new Uint8Array([0x00, 0x03]))
+                          console.log(controlCharacteristic.properties);
+                          //console.log("writing value to device");
+                          controlCharacteristic.startNotifications();
+                          controlCharacteristic.writeValueWithResponse(new Uint8Array([0x02, 0x03]))
+                          .then(_ => {
+                            console.log(controlCharacteristic.readValue());
+                          })
 
 
                         })
@@ -145,72 +185,72 @@ const onClickEvent = () => {
   }
   function batteryLevelChanged(event){
     console.log(event);
-  }
+  } */
 }
 
-/*   const connectToDeviceAndSubscribeToUpdates = async () => {
-    try {
-      // Search for Bluetooth devices that advertise a battery service
-      const device = await navigator.bluetooth
-        .requestDevice(options);
 
-      setIsDisconnected(false);
+function testi(event){
+  let commandValue = event.target.value.getUint8(0);
+  console.log(event);
+}
 
-      // Add an event listener to detect when a device disconnects
-      device.addEventListener('gattserverdisconnected', onDisconnected);
-
-      // Try to connect to the remote GATT Server running on the Bluetooth device
-      const server = await device.gatt.connect();
-
-      // Get the battery service from the Bluetooth device
-      const batService = await server.getPrimaryService('battery_service');
-
-      const accService = await server.getPrimaryService('fb005c80-02e7-f387-1cad-8acd2d8df0c8');
-
-      // Get the battery level characteristic from the Bluetooth device
-      const characteristic = await batService.getCharacteristic('battery_level');
-
-      const pmdControlCharacteristic = await accService.getCharacteristic('fb005c81-02e7-f387-1cad-8acd2d8df0c8');
-
-      const pmdDataCharacteristic = await accService.getCharacteristic('fb005c82-02e7-f387-1cad-8acd2d8df0c8');
-
-      pmdControlCharacteristic.writeValueWithResponse(new Uint8Array([0x02, 0x03]));
-
-      pmdDataCharacteristic.startNotifications();
-
-      pmdDataCharacteristic.addEventListener('characteristicValueChanged',
-        handleCharacteristicValueChanged);
-
-      // Subscribe to battery level notifications
-      characteristic.startNotifications();
-
-      // When the battery level changes, call a function
-      characteristic.addEventListener('characteristicvaluechanged',
-        handleCharacteristicValueChanged); 
-
-      // Read the battery level value
-      const reading = await characteristic.readValue();
-      const readingAcc = await pmdControlCharacteristic.readValue();
-      setAcceleration(readingAcc.getUint8(1));
-
-      // Show the initial reading on the web page
-      setBatteryLevel(reading.getUint8(0) + '%');
-    } catch (error) {
-      console.log(`There was an error: ${error}`);
+/* const startStream = () => {
+  console.log(device);
+  console.log(server);
+  console.log(services);
+  console.log("täällä");
+  server.getPrimaryService(Heart_rate_Service)
+  .then(value => {
+    value.getCharacteristic(Heart_rate_Char)
+    .then(value => {
+      value.startNotifications();
+      value.addEventListener("value changed", (e) => heartRateChanged(e));
+    })
+  })
+  services.forEach(element => {
+    if (element.uuid === PMD_Service) {
+      element.getCharacteristic(Cntrl_char)
+      .then(controlChar => {
+        console.log(controlChar.properties);
+        controlChar.writeValueWithResponse(new Uint8Array([0x02, 0x03]))
+        .then(_ => {
+          console.log(controlChar.readValue());
+          return;
+        })
+      })
+      .then(_ => {
+        element.getCharacteristic(Data_char)
+        .then(dataChar => {
+          dataChar.startNotifications();
+          return dataChar;
+        })
+        .then(dataChar => {
+          console.log("notifications started");
+          dataChar.addEventListener("value_changed", testi());
+        });
+      });
+    } if (element.uuid === Heart_rate_Service) {
+        element.getCharacteristic(Heart_rate_Char)
+        .then(heartRateChar => {
+          console.log(heartRateChar);
+          heartRateChar.startNotifications();
+          heartRateChar.addEventListener("value_changed", (event => {
+            console.log(event.target.value);
+          }));
+        })
     }
-  }; */
+  });
+} */
 
-
-  
-  
 
   return (
 <div class="back">
 
 <div class="header">
     <img class="logo" src="/polarLogo.png"/>
-    <button onClick={onClickEvent} id="connectButton">Connect device</button>
+    <button onClick={connectDevice} id="connectButton">Connect device</button>
     <button onClick={onDisconnectedButtonClick} class="disConnectButton">Disconnect device</button>
+    {/* <button onClick={startStream} class="disConnectButton">Start acc</button> */}
 </div>
 <hr/>
 <div class="stats-container">
