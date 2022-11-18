db3c4a8aea6afb0eb45b94043a9f28c23e076972
t0viti00 <78855803+t0viti00@users.noreply.github.com>
2022-11-18 11:01:23 +0200

    Menu.js update.

--- react_project/src/components/Menu.js
+++ react_project/src/components/Menu.js
@@ -106,17 +106,14 @@ export function Menu() {
     
     return(
         
-    
+    <div className={ styles.backgroundImage}>
         <div className={ styles.container }>
             <div className={ styles.logo }></div>
             <Clock />
-            <div className={ styles.device_container }>
-                <div className={ styles.device }>SENSOR</div>
-                <div className={ styles.device }>SENSOR</div>
-            </div>
-            <div className={ styles.select }>Choose Your Device</div>
+            
+            
             <button onClick={onClickEvent} className={ styles.button } >Connect Device</button>
-        </div>
+        </div></div>
   
     
     ) 
