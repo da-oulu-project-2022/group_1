56e07a72e9406ea003b7aa7791e8f0300361cdd2
Danskutin <dante.leino@gmail.com>
2022-11-25 23:28:50 +0200

    Conditional routing works at basic level

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-
-import { Menu } from './components/Menu'
+import Menu from './components/Menu'
+import { isConnected } from './components/Menu'
 
 
 function App() {
+
+  const isConnectionEstablished = isConnected
+  console.log(isConnectionEstablished)
+
+  // isConnectionEstablished == true &&
+
   return (
     <>
       <Routes>
         <Route path="/" element= { <Menu/> } />
+        { 
         <Route path="/HomePage" element= { <HomePage/> } />
+        }
         {/* <Route path="/signup" element= { <SignUp/> } /> */}
       </Routes>
     </>
