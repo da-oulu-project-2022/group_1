d016c0bd6fcc3cf67a75b3a4d3b674fde74f6989
Kooalaa <konsta.alajarvi@gmail.com>
2022-12-14 11:42:26 +0200

    Added some more comments to VeritySense.js and removed unsused code

--- react_project/src/components/VeritySense.js
+++ react_project/src/components/VeritySense.js
@@ -2,7 +2,7 @@ import Clock from './Clock';
 import { IotChart } from './Chart';
 import styles from './modules/VeritySense.module.css';
 import clockStyles from './modules/Clock.module.css';
-import React, { useEffect, useRef, useState } from 'react';   
+import React, { useEffect, useState } from 'react';   
 import { GoAlert } from "react-icons/go";
 
 function VeritySense(props) {
@@ -17,87 +17,41 @@ function VeritySense(props) {
   const Heart_rate_Char = "00002a37-0000-1000-8000-00805f9b34fb";
   const Battery_Char = "00002a19-0000-1000-8000-00805f9b34fb";
 
-  // Initializing some constant command arrays for PMD Control point to start measurements
-  const ACC_Array = new Uint8Array([0x02, 0x02, 0x00, 0x01, 0x34, 0x00, 0x01, 0x01, 0x10, 0x00, 0x02, 0x01, 0x08, 0x00, 0x04, 0x01, 0x03]);
-  const PPI_Array = new Uint8Array([0x02, 0x03]);
-
   let bpm_normal;
   let bpm_high;
   let bpm_low;
   let alert_box;
   let [bpm_now, setBpm] = useState();
-  let [ppi_now, setPpi] = useState();
 
+  // Some state variables used in change theme feature
   let [style, setStyle] = useState(styles.body);
   let [theme, setTheme] = useState('light');
   let [containerStyle, setContainerStyle] = useState(styles.dataContainer);
-
   let [dataUnit, setDataUnit] = useState(styles.dataUnit);
 
   let lowest_bpm;
   let highest_bpm;  
 
+  // UseEffect hook for initializing some html elements and starting the measurements on polar sensor
+  // runs only on the first render by declaring an empty array dependency as second parameter
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     bpm_normal = document.getElementById("bpm_normal");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     bpm_high = document.getElementById("bpm_high");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     bpm_low = document.getElementById("bpm_low");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     alert_box = document.getElementById("alertbox");
     startMeasurement();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  //Update the value shown on the web page when a notification is received.
 
   const handleBatteryValueChanged = (event) => {
     //setBatteryLevel(event.target.value.getUint8(0) + '%');
     console.log("Batterylevel: " + event.target.value.getUint8(0) + '%');
   }
 
-  const handlePmdDataValueChanged = (event) => {
-    if (event.target.value.getUint8(0) === 2) {
-  /* console.log("\n\nNew values");
-      console.log(event.target.value);
-
-      let ref_x = event.target.value.getInt8(11) * 256 + event.target.value.getInt8(10);
-      let ref_y = event.target.value.getInt8(13) * 256 + event.target.value.getInt8(12);
-      let ref_z = event.target.value.getInt8(15) * 256 + event.target.value.getInt8(14);
-      console.log("x " + ref_x + " y " + ref_y + " z " + ref_z );
-
-      let sample_x = ref_x + event.target.value.getInt8(18);
-      let sample_y = ref_y + event.target.value.getInt8(19);
-      let sample_z = ref_z + event.target.value.getInt8(20);
-      console.log("x " + sample_x + " y " + sample_y + " z " + sample_z );
-      
-      
-      for(let i = 21; i < 222; i=i+3){
-
-        sample_x = sample_x + event.target.value.getInt8(i);
-        sample_y = sample_y + event.target.value.getInt8(i+1);
-        sample_z = sample_z + event.target.value.getInt8(i+2); 
-        if (i%30 == 0) {
-          console.log("x " + sample_x + " y " + sample_y + " z " + sample_z );
-        }
-      } */
-      
-    } else if (event.target.value.getUint8(0) === 3) {
-      let sample = pasrseToInt16(event.target.value.buffer.slice(11, 13));
-      console.log(sample);
-      setPpi(sample);
-    }
-  }
-
-  function pasrseToInt16(byte_array){
-    let m_array = new Uint8Array(byte_array)
-    let value0 = m_array[0].toString(16)
-    if (value0.length == 1){ value0 = 0 + value0}
-    let value1 = m_array[1].toString(16)
-    if (value1.length == 1){ value1 = 0 + value1}
-    let value = value1 + value0;
-    value = parseInt(value, 16);
-
-    if (value > 32767) { value = value - 65536 };
-    return value;
-  }
 
   // Handling the event of heartrate service value changing
   // firstly setting state variable bpm_now with setBpm to current the current event.target.value
@@ -107,17 +61,13 @@ function VeritySense(props) {
   const handleHRValueChanged = (event) => {
     setBpm(event.target.value.getUint8(1));
     bpm_normal.innerText = event.target.value.getUint8(1);
-      
     if (lowest_bpm == undefined || lowest_bpm > event.target.value.getUint8(1)){
       lowest_bpm = event.target.value.getUint8(1);
       bpm_low.innerText = event.target.value.getUint8(1);
-        
     }
-
     if (highest_bpm == undefined || highest_bpm < event.target.value.getUint8(1)){
       highest_bpm = event.target.value.getUint8(1);
       bpm_high.innerText = event.target.value.getUint8(1);
-        
     }
       if (event.target.value.getUint8(1) > 100){
         alert_box.style.display = "flex";
@@ -133,26 +83,14 @@ function VeritySense(props) {
     props.device.gatt.getPrimaryServices()
     .then(services => { 
       services.forEach(element => {
-  /*       if (element.uuid === PMD_Service) {
-          element.getCharacteristic(Data_char).then(dataChar => {
-            dataChar.startNotifications();
-            dataChar.addEventListener("characteristicvaluechanged", handlePmdDataValueChanged);
-          }).then(_ => {
-            element.getCharacteristic(Cntrl_char)
-            .then(controlChar => {
-              controlChar.writeValueWithResponse(PPI_Array)
-              .then(_ => {
-                controlChar.writeValueWithResponse(ACC_Array);
-              });
-            })
-          })
-        } */ if (element.uuid === Heart_rate_Service) {
+        if (element.uuid === Heart_rate_Service) {
             element.getCharacteristic(Heart_rate_Char)
             .then(heartRateChar => {
               heartRateChar.startNotifications();
               heartRateChar.addEventListener("characteristicvaluechanged", handleHRValueChanged);
             })
-        } if (element.uuid === Battery_Service) {
+        } 
+        if (element.uuid === Battery_Service) {
             element.getCharacteristic(Battery_Char)
             .then(char => {
               char.readValue()
@@ -211,7 +149,6 @@ function VeritySense(props) {
               <div className={ styles.graph }>
               <IotChart data={bpm_now}/>
               </div>
-              <p>{ppi_now}</p>
           </section>
           <button style={{height: '100px', marginLeft: '440px', marginBottom: '440px', width: '100px'}} onClick={handleStyleChange}> Change theme </button>
         </div>
